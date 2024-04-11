aa0d5ab6f0c56f7f639f4f24512376882c153bda
Ibsham Chaudhary <progwithibi@gmail.com>
2024-04-11 22:28:22 +0500

    Creating Context with JSX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+1- Import React and ReactDOM Libraries 
+2- Get a reference to div with id "root"
+3- Tell React to take control of that component 
+4- Create a component 
+5- Show the component on screen 
+ */
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+const el = document.querySelector("#root");
+const root = ReactDOM.createRoot(el);
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
